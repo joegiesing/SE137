ec26188fce02439735ce559956c1cfa4e69930f3
joegiesing <jrgiesing@email.neit.edu>
2022-05-16 08:02:40 -0400

    w6

---
+++ w6/script.js
@@ -0,0 +1,24 @@
+var tray = document.querySelector('#tray');
+
+tray.addEventListener('click', openClose)
+
+function openClose(e)
+{
+    var nav = document.querySelector('nav');
+    nav.classList.toggle('hidden');
+}
+
+var tabs = document.querySelectorAll('#tabs a');
+console.log(tabs);
+
+for(let i=0; i < tabs.length; i++)
+{
+    tabs[i].addEventListener('click', function(e){
+        for(let i=0; i < tabs.length; i++)
+        {
+            tabs[i].getElementsByClassName.backgroundcolor = 'orange';
+        }
+        e.target.style.backgroundcolor = '#161616'
+        document.querySelectorAll('#breadcrumbs a')[3].innerHTML = e.target.innerHTML
+    });
+}
